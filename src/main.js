f31f30b47a866e518b5b56e8865662d546e8e51f
taliakatz <taliakat@post.bgu.ac.il>
2021-04-27 10:23:59 +0300

    update 27.4
    update

--- src/main.js
+++ src/main.js
@@ -5759,19 +5759,6 @@
     if (!supports) {
         return
     };
-    // document.write('<script type="text/javascript" src="test.js" ></script>');
-
-    //   main.load = function (context, moduleName, url) {
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open("GET", url, true);
-    //     xhr.onreadystatechange = function (e) {
-    //       if (xhr.readyState === 4 && xhr.status === 200) {
-    //         eval(xhr.responseText);
-    //         context.completeLoad(moduleName)
-    //       }
-    //     };
-    //     xhr.send(null);
-    //   };
 
     //Libraries
     //XRegExp 2.0.0 <xregexp.com> MIT License
@@ -5867,25 +5854,11 @@
         }
     };
 
-    ////
+
     var hidePopup = function () {
         popUpElem.style.display = "none";
     };
-    ////
 
-    // const { DOMParser } = require('xmldom');
-    // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.open("GET", "http://knesset.gov.il/Odata/ParliamentInfo.svc/KNS_LawBinding?$filter=%20LawID%20eq%20(5)&$select=IsraelLawID", false); // false for synchronous request
-    // xmlHttp.send(null);
-    // console.log(xmlHttp.responseText);
-    // var ans = xmlHttp.responseText;
-    // let parser = new DOMParser();
-    // let xmlDoc = parser.parseFromString(ans, "text/xml");
-    // var num = xmlDoc.getElementsByTagNameNS("http://schemas.microsoft.com/ado/2007/08/dataservices", "IsraelLawID")[0].firstChild.nodeValue;
-    // // var allParas = document.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "p");
-    // let str = "https://main.knesset.gov.il/Activity/Legislation/Laws/Pages/LawPrimary.aspx?t=lawlaws&st=lawlaws&lawitemid=" + num;
-    // console.log(str);
 
     ns.matches = [];
     ns.sources = {};
